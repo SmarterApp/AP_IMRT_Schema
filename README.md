5d4ae2898bf0c62997a4b3b360f578e900de87dd
swilson11 <sue.wilson4@gmail.com>
2018-02-23 14:16:49 -0800

    Initial database schema with flyway gradle integration

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # AP_IMRT_Schema
 Contains SQL schema migrations to support the IMRT application(s)
+
+Uses the flyway gradle plugin: https://flywaydb.org/documentation/gradle/
+
+Assumes that the database itself has already been created and configured
+
+To use, configuir the following properties, either via environment variables, command line arguments, are gradle.properties:
+<pre>
+flyway.user=<user>
+flyway.password=<password>
+flyway.url=<url> 
+Sample URL for local database named imrt: jdbc:postgresql://localhost:5432/imrt
+</pre>
+
+Then run the flyway tasks, for example:
+<pre>
+./gradlew flywayClean
+./gradlew flywayMigrate
+</pre>
+
+## SQL Conventions
+
+SQL files should conform to the following conventions:
+* Use all lowercase letters for names
+* Separate words using underscores
+* Foreign key names should follow the format \<fk table>_\<fk column>
+* Timestamp field names should end is _at, for example 'created_at'
+* Timestamp columns should use the timestamptz type
+* Columns denoting authorship should end in _by, for example 'updated_by'
+
+## Migrations
+
+* Migrations scripts should be added to the directory src/main/resources/db/migration
+* Migration scripts should be named according to the flyway naming conventions definted here: 
+https://flywaydb.org/documentation/migrations#naming
+* The "Description" component of the file name should end with either '_ddl' (data definition language)
+ or '_dml' (data manipulation languge), for example V1_imrt_ddl.sql  
+
+
+
---
+++ src/main/resources/db/migration/V1__imrt_ddl.sql
@@ -0,0 +1,70 @@
+/* Primary table containing all types of items and the json data from IAT */
+CREATE TABLE item (
+  key             SERIAL PRIMARY KEY, -- Primary key for items, auto-incrementing (change serial to AUTO_INCREMENT for mySQL)
+  id              VARCHAR(9)  NOT NULL, -- Item ID as specified by IAT
+  subject         VARCHAR(20) NOT NULL,
+  grade           VARCHAR     NOT NULL,
+  workflow_status VARCHAR     NOT NULL,
+  item_type       VARCHAR     NOT NULL,
+  dok             VARCHAR     NOT NULL,
+  item_created_by VARCHAR     NOT NULL,
+  item_created_at VARCHAR     NOT NULL,
+  item_json       JSON        NOT NULL,
+  created_at      TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_at      TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by      VARCHAR     NOT NULL
+);
+
+CREATE TABLE standard_id (
+  item_key             INT         NOT NULL,
+  type                 INT         NOT NULL, -- Primary = 1, Secondary = 2, etc
+  claim                VARCHAR     NOT NULL,
+  content_domain       VARCHAR     NOT NULL,
+  target               VARCHAR     NOT NULL,
+  emphasis             VARCHAR     NOT NULL,
+  common_core_standard VARCHAR     NOT NULL,
+  created_at           TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_at           TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by           VARCHAR     NOT NULL,
+  FOREIGN KEY (item_key) REFERENCES item (key),
+  PRIMARY KEY (item_key, type)
+);
+
+/* Git Information associated with each item */
+CREATE TABLE item_git (
+  item_key            INT         NOT NULL,
+  project_id          INT PRIMARY KEY, -- Unique identifier from GIT
+  project_path        VARCHAR     NOT NULL, -- Full path including namespace (group)
+  current_commit_hash VARCHAR     NOT NULL,
+  created_at          TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_at          TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by          VARCHAR     NOT NULL,
+  FOREIGN KEY (item_key) REFERENCES item (key),
+  UNIQUE (item_key, project_id)
+);
+
+/* Historical item data */
+CREATE TABLE item_log (
+  item_key    INT         NOT NULL,
+  commit_hash VARCHAR     NOT NULL,
+  commit_date TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  created_at  TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by  VARCHAR     NOT NULL,
+  FOREIGN KEY (item_key) REFERENCES item (key),
+  PRIMARY KEY (item_key, commit_hash)
+);
+
+/* Link Assessment items and stimulii */
+CREATE TABLE stim_link (
+  item_key      INT         NOT NULL,
+  item_id       INT         NOT NULL,
+  item_key_stim INT         NOT NULL,
+  item_id_stim  INT         NOT NULL,
+  created_at    TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_at    TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by    VARCHAR     NOT NULL,
+  FOREIGN KEY (item_key) REFERENCES item (key),
+  FOREIGN KEY (item_key_stim) REFERENCES item (key),
+  PRIMARY KEY (item_key, item_key_stim)
+);
