1cfb1725c3e56c450f066274425c3e5b1b126f02
Todd Miller <tmiller@fairwaytech.com>
2018-04-05 14:42:54 -0700

    Set the proper width for the associated stimulus column

--- src/main/resources/db/migration/V5__imrt_ddl.sql
+++ src/main/resources/db/migration/V5__imrt_ddl.sql
@@ -2,4 +2,4 @@
 * Add associated stimulus id to the item table
 ******************************************************************************/
 
-ALTER TABLE item ADD COLUMN associated_stimulus_id character varying(30);
+ALTER TABLE item ADD COLUMN associated_stimulus_id character varying(40);
