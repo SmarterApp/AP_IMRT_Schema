3b1b879a5b5b64cc2aca768fc4bd87df87c728cb
Todd Miller <tmiller@fairwaytech.com>
2018-04-07 12:43:34 -0700

    Re-add the time in workflow schema change

--- src/main/resources/db/migration/V6__imrt_ddl.sql
+++ src/main/resources/db/migration/V6__imrt_ddl.sql
@@ -1,6 +1,7 @@
 /******************************************************************************
 * Add the time the workflow status was set to the item table
 ******************************************************************************/
+
 /* Use default for existing rows */
 ALTER TABLE item ADD COLUMN workflow_status_set_at TIMESTAMPTZ NOT NULL DEFAULT timestamp with time zone '1970-01-01';
 
