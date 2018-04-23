c802541cae64273ecc6efa8f226e584c5179c79e
Todd Miller <tmiller@fairwaytech.com>
2018-04-23 09:21:33 -0700

    Remove sizing restrtiction as IAT doesn't limit fields

--- src/main/resources/db/migration/V12__imrt_ddl.sql
+++ src/main/resources/db/migration/V12__imrt_ddl.sql
@@ -2,4 +2,4 @@
  * Add item author field
  ****************************************************/
 
-alter table item add column item_author character varying(100) NOT NULL DEFAULT '';
+alter table item add column item_author character varying NOT NULL DEFAULT '';
