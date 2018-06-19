5e7af322e5ed87123c9f5a46ca5c304f94460f28
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-06-18 18:27:44 -0700

    IMRT-416: Add TTS fields and data migration

---
+++ src/main/resources/db/migration/V17__imrt_ddl.sql
@@ -0,0 +1,14 @@
+/*******************************************************************
+* IMRT-416: Adds TTS provided/required fields to item table
+********************************************************************/
+ALTER TABLE item ADD COLUMN sight_tts_provided VARCHAR NOT NULL DEFAULT '';
+ALTER TABLE item ADD COLUMN visual_tts_provided VARCHAR NOT NULL DEFAULT '';
+ALTER TABLE item ADD COLUMN visual_tts_required VARCHAR NOT NULL DEFAULT '';
+
+-- Populate new fields with ingested item.json data
+UPDATE
+	item
+SET
+	sight_tts_provided = item_json->'textToSpeech'->>'isSightTTSProvided',
+	visual_tts_provided = item_json->'textToSpeech'->>'isVisualTTSProvided',
+	visual_tts_required = item_json->'textToSpeech'->>'isVisualTTSRequired'
