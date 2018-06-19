a24a6f7d02913baf1b53f00e1a36739afe025b38
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-06-19 12:38:07 -0700

    IMRT-416: Change %Provided fields to boolean from text

--- src/main/resources/db/migration/V17__imrt_ddl.sql
+++ src/main/resources/db/migration/V17__imrt_ddl.sql
@@ -1,14 +1,14 @@
 /*******************************************************************
 * IMRT-416: Adds TTS provided/required fields to item table
 ********************************************************************/
-ALTER TABLE item ADD COLUMN sight_tts_provided VARCHAR NOT NULL DEFAULT '';
-ALTER TABLE item ADD COLUMN visual_tts_provided VARCHAR NOT NULL DEFAULT '';
+ALTER TABLE item ADD COLUMN sight_tts_provided BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN visual_tts_provided BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE item ADD COLUMN visual_tts_required VARCHAR NOT NULL DEFAULT '';
 
 -- Populate new fields with ingested item.json data
 UPDATE
 	item
 SET
-	sight_tts_provided = item_json->'textToSpeech'->>'isSightTTSProvided',
-	visual_tts_provided = item_json->'textToSpeech'->>'isVisualTTSProvided',
+	sight_tts_provided = (item_json->'textToSpeech'->>'isSightTTSProvided')::boolean,
+	visual_tts_provided = (item_json->'textToSpeech'->>'isVisualTTSProvided')::boolean,
 	visual_tts_required = item_json->'textToSpeech'->>'isVisualTTSRequired';
