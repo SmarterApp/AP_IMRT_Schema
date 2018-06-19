cf725f7e220491c3b1fdb6348fedd9e30a70dab0
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-06-19 13:47:57 -0700

    IMRT-416: make %Provided fields nullable

--- src/main/resources/db/migration/V17__imrt_ddl.sql
+++ src/main/resources/db/migration/V17__imrt_ddl.sql
@@ -1,14 +1,6 @@
 /*******************************************************************
 * IMRT-416: Adds TTS provided/required fields to item table
 ********************************************************************/
-ALTER TABLE item ADD COLUMN sight_tts_provided BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE item ADD COLUMN visual_tts_provided BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN sight_tts_provided BOOLEAN;
+ALTER TABLE item ADD COLUMN visual_tts_provided BOOLEAN;
 ALTER TABLE item ADD COLUMN visual_tts_required VARCHAR NOT NULL DEFAULT '';
-
--- Populate new fields with ingested item.json data
-UPDATE
-	item
-SET
-	sight_tts_provided = (item_json->'textToSpeech'->>'isSightTTSProvided')::boolean,
-	visual_tts_provided = (item_json->'textToSpeech'->>'isVisualTTSProvided')::boolean,
-	visual_tts_required = item_json->'textToSpeech'->>'isVisualTTSRequired';
