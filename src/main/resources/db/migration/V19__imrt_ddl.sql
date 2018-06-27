632322c3c4367b757d9803643853f7b2092ba5e6
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-06-27 12:21:08 -0700

    IMRT-439: rename migration

--- src/main/resources/db/migration/V19__imrt_ddl.sql
+++
@@ -1,9 +0,0 @@
-/*******************************************************************
-* IMRT-439: Update primary key constraint for attachments to include
-* file_type
-********************************************************************/
-ALTER TABLE item_attachment
-	DROP CONSTRAINT item_attachment_pkey;
-
-ALTER TABLE item_attachment
-	ADD CONSTRAINT item_attachment_pkey PRIMARY KEY(item_key, file_name, file_type);
