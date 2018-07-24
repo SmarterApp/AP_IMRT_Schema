f7916885d5d7e45d683c28642136f539ed8ab178
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-07-24 13:23:34 -0700

    IAT-2040: make 'language updated at' columns nullable and w/o defaults

--- src/main/resources/db/migration/V24__imrt_ddl.sql
+++ src/main/resources/db/migration/V24__imrt_ddl.sql
@@ -3,4 +3,11 @@
 ********************************************************************/
 
 ALTER TABLE item
-  ADD COLUMN spanish_content_last_updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp;
+  ADD COLUMN spanish_content_last_updated_at TIMESTAMPTZ NULL;
+
+-- make english_content_last_updated_at nullable and remove the default
+ALTER TABLE item
+  ALTER COLUMN english_content_last_updated_at DROP NOT NULL;
+
+ALTER TABLE item
+  ALTER COLUMN english_content_last_updated_at DROP DEFAULT;
