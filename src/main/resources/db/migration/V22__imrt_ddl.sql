11f9e2843381b312b02216b3c3fbbafff9318b25
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-07-23 17:48:47 -0700

    IAT-2038: rename column for consistency throughout apps

--- src/main/resources/db/migration/V22__imrt_ddl.sql
+++ src/main/resources/db/migration/V22__imrt_ddl.sql
@@ -3,4 +3,4 @@
 ********************************************************************/
 
 ALTER TABLE item
-  ADD COLUMN english_last_updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp;
+  ADD COLUMN english_content_last_updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp;
