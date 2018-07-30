38708239fb71292ac69319d7884fb7f135156e42
Todd Miller <millerts.git@gmail.com>
2018-07-30 14:40:25 -0700

    Use allow calculator

--- src/main/resources/db/migration/V26__imrt_ddl.sql
+++ src/main/resources/db/migration/V26__imrt_ddl.sql
@@ -2,4 +2,4 @@
 * Add allow calculator
 ********************************************************************/
 
-ALTER TABLE item ADD COLUMN calculator VARCHAR NOT NULL DEFAULT ''
+ALTER TABLE item ADD COLUMN allow_calculator VARCHAR NOT NULL DEFAULT ''
