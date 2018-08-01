fd9323e62d1b2ab73398fcae83af16e53f6b7698
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-07-31 17:39:05 -0700

    IAT-2039: Add fields for each type of attachment

--- src/main/resources/db/migration/V27__imrt_ddl.sql
+++ src/main/resources/db/migration/V27__imrt_ddl.sql
@@ -2,4 +2,6 @@
 * Add attachments uploaded after english last updated
 ********************************************************************/
 
-ALTER TABLE item ADD COLUMN attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN asl_attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN braille_attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN cc_attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
