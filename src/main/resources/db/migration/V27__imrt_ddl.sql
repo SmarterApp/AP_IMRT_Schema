f091ccc7889c4a9fc179577f32027a1a19b712cc
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-08-01 08:37:25 -0700

    IAT-2039: Rename fields

--- src/main/resources/db/migration/V27__imrt_ddl.sql
+++ src/main/resources/db/migration/V27__imrt_ddl.sql
@@ -2,6 +2,6 @@
 * Add attachments uploaded after english last updated
 ********************************************************************/
 
-ALTER TABLE item ADD COLUMN asl_attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE item ADD COLUMN braille_attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE item ADD COLUMN cc_attachments_changed_after_english_updated BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN asl_uploaded_prior_to_last_content_update BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN braille_uploaded_prior_to_last_content_update BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN closed_captioning_uploaded_prior_to_last_content_update BOOLEAN NOT NULL DEFAULT FALSE;
