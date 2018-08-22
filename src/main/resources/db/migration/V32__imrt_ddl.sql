154d390f407543090959661ded398cc2e895be73
Todd Miller <millerts.git@gmail.com>
2018-08-21 17:24:00 -0700

    Renamed file to account for other migration merged

--- src/main/resources/db/migration/V32__imrt_ddl.sql
+++ src/main/resources/db/migration/V32__imrt_ddl.sql
@@ -1,12 +1,18 @@
 /**
- * Create view to contain the latest content update need by created at
+ * Create the content update needs
  */
 
-CREATE VIEW latest_item_content_update_need AS
-  SELECT item_content_update_need.*
-  FROM item_content_update_need
-  INNER JOIN (
-               select item_key, MAX(content_update_need_create_date) as created_at from item_content_update_need group by item_key
-  ) as latest_record
-  ON latest_record.item_key = item_content_update_need.item_key
-  AND latest_record.created_at = item_content_update_need.content_update_need_create_date;
+CREATE TABLE item_content_update_need (
+  key SERIAL PRIMARY KEY,
+  item_key                        INT NOT NULL REFERENCES item,
+  internal_resolution             VARCHAR,
+  notes                           VARCHAR,
+  resolution                      VARCHAR,
+  resolution_notes                VARCHAR,
+  content_update_need_create_date TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  internal_resolution_date        TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  resolution_date                 TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  created_at                      TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_at                      TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by                      VARCHAR NOT NULL
+);
