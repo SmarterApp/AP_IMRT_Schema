0b411e34db8b221721eec16c41c495087f407632
Todd Miller <millerts.git@gmail.com>
2018-08-21 17:29:15 -0700

    Alter the direction of the current content need data after discussion with team.

--- src/main/resources/db/migration/V32__imrt_ddl.sql
+++ src/main/resources/db/migration/V32__imrt_ddl.sql
@@ -16,3 +16,10 @@ CREATE TABLE item_content_update_need (
   updated_at                      TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
   updated_by                      VARCHAR NOT NULL
 );
+
+ALTER TABLE item ADD COLUMN unresolved_update_need boolean NOT NULL DEFAULT FALSE;
+ALTER TABLE item ADD COLUMN current_update_need_create_date TIMESTAMPTZ;
+ALTER TABLE item ADD COLUMN current_update_need_internal_resolution_date TIMESTAMPTZ;
+ALTER TABLE item ADD COLUMN current_update_need_internal_resolution VARCHAR NOT NULL DEFAULT '';
+ALTER TABLE item ADD COLUMN current_update_need_resolution_date TIMESTAMPTZ;
+ALTER TABLE item ADD COLUMN current_update_need_resolution VARCHAR NOT NULL DEFAULT '';
