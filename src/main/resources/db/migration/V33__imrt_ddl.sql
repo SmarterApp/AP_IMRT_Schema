6a823592ef18026774a66e1b6216a1e0c8341c7a
Todd Miller <millerts.git@gmail.com>
2018-08-27 12:51:27 -0700

    Set glossary fields to boolean which should be boolean

--- src/main/resources/db/migration/V33__imrt_ddl.sql
+++ src/main/resources/db/migration/V33__imrt_ddl.sql
@@ -3,12 +3,12 @@
  **************************************************************/
 
 ALTER TABLE item
-  ADD COLUMN english_glossary_provided VARCHAR NOT NULL DEFAULT '';
+  ADD COLUMN english_glossary_provided boolean NOT NULL DEFAULT FALSE;
 ALTER TABLE item
-  ADD COLUMN illustrated_glossary_provided VARCHAR NOT NULL DEFAULT '';
+  ADD COLUMN illustrated_glossary_provided boolean NOT NULL DEFAULT FALSE;
 ALTER TABLE item
   ADD COLUMN illustrated_glossary_required VARCHAR NOT NULL DEFAULT '';
 ALTER TABLE item
-  ADD COLUMN translated_glossary_provided VARCHAR NOT NULL DEFAULT '';
+  ADD COLUMN translated_glossary_provided boolean NOT NULL DEFAULT FALSE;
 ALTER TABLE item
   ADD COLUMN translated_glossary_required VARCHAR NOT NULL DEFAULT '';
