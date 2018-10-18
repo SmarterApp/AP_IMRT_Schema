41f9a2894cdbf660cdd5d4cd3b78fcf95d2212e7
Greg Charles <gcharles@fairwaytech.com>
2018-10-18 12:16:58 -0700

    IAT-2432: Remove trigram index for now.

--- src/main/resources/db/migration/V38__imrt_ddl.sql
+++ src/main/resources/db/migration/V38__imrt_ddl.sql
@@ -4,6 +4,3 @@
  * index to item_keyword content
  ***************************************************/
 ALTER TABLE item_keyword ALTER COLUMN section SET DATA TYPE varchar(60);
-
-CREATE EXTENSION pg_trgm;
-CREATE INDEX idx_trgm_item_keyword_content ON item_keyword USING GIN (content gin_trgm_ops);
